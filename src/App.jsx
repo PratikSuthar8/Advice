f13ce41a33b273ebcaa89b046784caf51b954ed5
Pratik Suthar <d365technical@dynamicdaytechnologies.com>
2024-05-21 14:27:33 +0530

    Create: structure for advice app created

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,24 @@
+import { useState } from "react";
 import "./App.css";
-
 function App() {
-	return <></>;
+	const [advice, setAdvice] = useState("");
+	const [count, setCount] = useState(0);
+	async function getAdvice() {
+		const res = await fetch("https://api.adviceslip.com/advice");
+		const data = await res.json();
+		setAdvice(data.slip.advice);
+		setCount((c) => c + 1);
+	}
+	return (
+		<>
+			<h1>Get Advice</h1>
+			<button onClick={getAdvice}>Get Advice</button>
+			<h2>{advice}</h2>
+			<p>
+				You took <strong>{count}</strong> advice today
+			</p>
+		</>
+	);
 }
 
 export default App;
