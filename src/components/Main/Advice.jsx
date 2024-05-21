eca7f4ca3282b8067864788d83d3be26f8bf27e3
Pratik Suthar <d365technical@dynamicdaytechnologies.com>
2024-05-21 15:32:08 +0530

    Update: Advice.jsx removed advice counter

--- src/components/Main/Advice.jsx
+++ src/components/Main/Advice.jsx
@@ -2,13 +2,10 @@ import React, { useState } from "react";
 import "./Advice.css";
 const Advice = () => {
 	const [advice, setAdvice] = useState("");
-
-	const [count, setCount] = useState(0);
 	async function getAdvice() {
 		const res = await fetch("https://api.adviceslip.com/advice");
 		const data = await res.json();
 		setAdvice(data.slip.advice);
-		setCount((c) => c + 1);
 	}
 
 	return (
@@ -17,9 +14,6 @@ const Advice = () => {
 				<div className="advice-generator">
 					<h1>Today'sAdvice</h1>
 					<button onClick={getAdvice}>Get Advice</button>
-					<p className="count">
-						You took <strong>{count}</strong> advice.
-					</p>
 				</div>
 				<div className="advice">
 					<h2>{advice}</h2>
